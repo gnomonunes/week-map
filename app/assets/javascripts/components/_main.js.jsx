d10fff69615509bd22cf22b4f82f438a3aa43500
Fernando Nunes <gnomonunes@gmail.com>
2018-12-27 11:08:59 +0000

    List goals from API endpoint

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -1,12 +1,8 @@
 const Main = (props) => {
-    return(
-      <div>
-        <h1>Your current week map!</h1>
-        <h2>Week goals</h2>
-        <ul>
-          <li>finish project first feature</li>
-          <li>work out at least 3 day</li>
-        </ul>
-      </div>
-    )
+  return(
+    <div>
+      <h1>Your current week map!</h1>
+      <Goals />
+    </div>
+  )
 }
