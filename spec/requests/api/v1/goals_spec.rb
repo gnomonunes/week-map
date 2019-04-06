74839a8819e9739ec1bbc2f1f53ca692d7185354
Fernando Nunes <gnomonunes@gmail.com>
2019-04-06 19:37:34 +0100

    Add Goal requests spec cases

--- spec/requests/api/v1/goals_spec.rb
+++ spec/requests/api/v1/goals_spec.rb
@@ -1,48 +1,71 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Api::V1::Goals", type: :request do
-  let(:week) { FactoryBot.create(:week) }
-  let(:goal) { FactoryBot.create(:goal) }
   let(:goal_show_path_regex) { /\/api\/v1\/weeks\/\d+\/goals\/\d+/ }
+  let(:week) { FactoryBot.create(:week) }
+  let(:goal) do
+    FactoryBot.create(:goal, description: "do the laundry", week: week)
+  end
+
+  describe "GET /api/v1/weeks/:week_id/goals/:id" do
+    it "returns a goal" do
+      get "/api/v1/weeks/#{week.id}/goals/#{goal.id}"
+
+      expect(response).to have_http_status(200)
+      expect(response).to be_jsonapi_response_for("goal")
+      expect(JSON.parse(response.body)["data"]["attributes"]["description"])
+        .to eq("do the laundry")
+    end
+  end
 
   describe "POST /api/v1/weeks/:week_id/goals" do
-    # let(:week) { FactoryBot.create(:week) }
     it "creates a goal" do
       post "/api/v1/weeks/#{week.id}/goals", {
         params: {
           data: {
-            type: 'goal',
+            type: "goal",
             attributes: {
-              description: "water plants"
+              description: "finish React training"
             }
           }
         }
       }
 
       expect(response).to have_http_status(201)
-      expect(response.header['Location']).to match(goal_show_path_regex)
-      expect(response).to be_jsonapi_response_for('goal')
+      expect(response.header["Location"]).to match(goal_show_path_regex)
+      expect(response).to be_jsonapi_response_for("goal")
+      expect(JSON.parse(response.body)["data"]["attributes"]["description"])
+        .to eq("finish React training")
     end
   end
 
   describe "PUT /api/v1/weeks/:week_id/goals/:id" do
     it "updates a goal" do
-      # binding.pry
       put "/api/v1/weeks/#{week.id}/goals/#{goal.id}", {
         params: {
           data: {
             id: goal.id,
-            type: 'goal',
+            type: "goal",
             attributes: {
-              description: "water plants"
+              description: "finish Redux training"
             }
           }
         }
       }
 
       expect(response).to have_http_status(200)
-      expect(response.header['Location']).to match(goal_show_path_regex)
-      expect(response).to be_jsonapi_response_for('goal')
+      expect(response.header["Location"]).to match(goal_show_path_regex)
+      expect(response).to be_jsonapi_response_for("goal")
+      expect(JSON.parse(response.body)["data"]["attributes"]["description"])
+        .to eq("finish Redux training")
+    end
+  end
+
+  describe "delete /api/v1/weeks/:week_id/goals/:id" do
+    it "deletes a goal" do
+      delete "/api/v1/weeks/#{week.id}/goals/#{goal.id}"
+
+      expect(response).to have_http_status(204)
     end
   end
 end
